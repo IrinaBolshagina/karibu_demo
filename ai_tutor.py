7d2df89d3ef00c2782299cd8ece009e5a4ab3ad3
mhogg <mhogg@student.42.fr>
2024-12-03 11:48:04 +0100

    changed prompt formatting

--- ai_tutor.py
+++ ai_tutor.py
@@ -2,30 +2,32 @@ from huggingface_hub import InferenceClient
 
 
 def make_prompt(question, answer):
-    return f"""Tu es un enseignant qui doit évaluer la réponse d'un étudiant.
+    return f"""Évalue ma réponse.
+    Question : "{question}"
+    Ma réponse : "{answer}"
+    Évalue cette réponse en suivant exactement ce format :
 
-Question posée à l'étudiant : "{question}"
+    **Compréhension**
+    [Analyse si la réponse correspond à la question posée]
 
-Réponse de l'étudiant : "{answer}"
-
-Évalue cette réponse en suivant exactement ce format :
-
-**Compréhension**
-[Analyse si la réponse correspond à la question posée]
-
-**Vocabulaire**
-[Analyse du vocabulaire utilisé]
-
-**Grammaire**
-[Corrections nécessaires OU "Le texte est correct. Félicitations !"]
-
-**Appréciation générale**
-[Bref commentaire encourageant]
-
-Importante: Évalue uniquement le texte fourni, sans le réécrire ni en générer un nouveau.
-"""
+    **Vocabulaire**
+    [Analyse du vocabulaire utilisé]
+    
+    **Grammaire**
+    [Corrections nécessaires OU "Le texte est correct. Félicitations !"]
+    
+    **Appréciation générale**
+    [Bref commentaire encourageant]
+    
+    Importante: Évalue uniquement le texte fourni, sans le réécrire ni en générer un nouveau.
+    """
 
 def correct_text(question, answer, hf_token):
     client = InferenceClient(model="meta-llama/Meta-Llama-3-70B-Instruct", token=hf_token)
-    output = client.text_generation(make_prompt(question, answer), max_new_tokens=4000)
-    return output
+    output = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "Tu es un enseignant qui évalue ma réponse."},
+        {"role": "user", "content": make_prompt(question, answer)},
+    ],
+    max_tokens=1024,)
+    return output['choices'][0]['message']['content']
